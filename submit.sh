91ca354bb219c07efadd7ba9cc3c48cb85603472
esoreqMP <eyal.soreq@mpi-mail.mpg.de>
2021-02-02 12:31:53 +0200

    routine update on Tue Feb  2 10:31:53 UTC 2021

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 . ./env/bin/activate
 . ~/.bash_functions
 CURR_VERSION=$(awk '/version="/{print $1}' setup.py | tr -d '[version=",]')
-sed  's|'"$CURR_VERSION"'|'"$1"'|g' setup.py > setup.py
+sed -i 's|'"$CURR_VERSION"'|'"$1"'|g' setup.py
 python setup.py bdist_wheel
 twine upload --skip-existing dist/*
 
